c4bc970d80d13d3c345e5735931eca50965d7769
9ssi7 <info@ssibrahimbas.com>
2024-02-11 03:00:56 +0300

    refactor/project: add the skeleton of http api

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.20
 
 require (
 	github.com/cilloparch/cillop v0.0.9
+	github.com/gofiber/fiber/v2 v2.48.0
 	github.com/turistikrota/service.shared v0.1.4
 	go.mongodb.org/mongo-driver v1.12.1
 )
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -18,8 +20,15 @@ require (
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/montanaflynn/stats v0.7.0 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.2.1 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.48.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
--- server/http/api.go
+++ server/http/api.go
@@ -1,1 +1,99 @@
 package http
+
+import "github.com/gofiber/fiber/v2"
+
+func UserAddMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserAddSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserAddTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserUpdateMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserUpdateSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserUpdateTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserRemoveMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserRemoveSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func UserRemoveTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessAddMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessAddSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessAddTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessUpdateMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessUpdateSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessUpdateTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessRemoveMail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessRemoveSms(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func BusinessRemoveTelegram(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func AdminFilter(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func AdminDetail(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func AdminDetailByUser(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func AdminDetailByBusiness(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func GetBySelectedUser(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func GetBySelectedBusiness(ctx *fiber.Ctx) error {
+	return nil
+}
