3ad8d172f54b9e0fa6596c9ed2a80792d12dd17f
9ssi7 <info@ssibrahimbas.com>
2024-02-11 03:09:38 +0300

    refactor/project: fix the http routing

--- config/roles.go
+++ config/roles.go
@@ -2,7 +2,20 @@ package config
 
 import "github.com/turistikrota/service.shared/base_roles"
 
-type actorConfigRoles struct{}
+type actorConfigRoles struct {
+	AddMail        string
+	AddSms         string
+	AddTelegram    string
+	RemoveMail     string
+	RemoveSms      string
+	RemoveTelegram string
+	EditMail       string
+	EditSms        string
+	EditTelegram   string
+	View           string
+	List           string
+	Super          string
+}
 
 type businessRoles struct {
 	Super string
@@ -15,8 +28,21 @@ type roles struct {
 }
 
 var Roles = roles{
-	Roles:       base_roles.BaseRoles,
-	ActorConfig: actorConfigRoles{},
+	Roles: base_roles.BaseRoles,
+	ActorConfig: actorConfigRoles{
+		AddMail:        "notify_actor_config.add_mail",
+		AddSms:         "notify_actor_config.add_sms",
+		AddTelegram:    "notify_actor_config.add_telegram",
+		RemoveMail:     "notify_actor_config.remove_mail",
+		RemoveSms:      "notify_actor_config.remove_sms",
+		RemoveTelegram: "notify_actor_config.remove_telegram",
+		EditMail:       "notify_actor_config.edit_mail",
+		EditSms:        "notify_actor_config.edit_sms",
+		EditTelegram:   "notify_actor_config.edit_telegram",
+		View:           "notify_actor_config.view",
+		List:           "notify_actor_config.list",
+		Super:          "notify_actor_config.super",
+	},
 	Business: businessRoles{
 		Super: "business.super",
 	},
--- server/http/api.go
+++ server/http/api.go
@@ -2,98 +2,98 @@ package http
 
 import "github.com/gofiber/fiber/v2"
 
-func UserAddMail(ctx *fiber.Ctx) error {
+func (h srv) UserAddMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserAddSms(ctx *fiber.Ctx) error {
+func (h srv) UserAddSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserAddTelegram(ctx *fiber.Ctx) error {
+func (h srv) UserAddTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserUpdateMail(ctx *fiber.Ctx) error {
+func (h srv) UserUpdateMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserUpdateSms(ctx *fiber.Ctx) error {
+func (h srv) UserUpdateSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserUpdateTelegram(ctx *fiber.Ctx) error {
+func (h srv) UserUpdateTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserRemoveMail(ctx *fiber.Ctx) error {
+func (h srv) UserRemoveMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserRemoveSms(ctx *fiber.Ctx) error {
+func (h srv) UserRemoveSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func UserRemoveTelegram(ctx *fiber.Ctx) error {
+func (h srv) UserRemoveTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessAddMail(ctx *fiber.Ctx) error {
+func (h srv) BusinessAddMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessAddSms(ctx *fiber.Ctx) error {
+func (h srv) BusinessAddSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessAddTelegram(ctx *fiber.Ctx) error {
+func (h srv) BusinessAddTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessUpdateMail(ctx *fiber.Ctx) error {
+func (h srv) BusinessUpdateMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessUpdateSms(ctx *fiber.Ctx) error {
+func (h srv) BusinessUpdateSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessUpdateTelegram(ctx *fiber.Ctx) error {
+func (h srv) BusinessUpdateTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessRemoveMail(ctx *fiber.Ctx) error {
+func (h srv) BusinessRemoveMail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessRemoveSms(ctx *fiber.Ctx) error {
+func (h srv) BusinessRemoveSms(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func BusinessRemoveTelegram(ctx *fiber.Ctx) error {
+func (h srv) BusinessRemoveTelegram(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func AdminFilter(ctx *fiber.Ctx) error {
+func (h srv) AdminFilter(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func AdminDetail(ctx *fiber.Ctx) error {
+func (h srv) AdminDetail(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func AdminDetailByUser(ctx *fiber.Ctx) error {
+func (h srv) AdminDetailByUser(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func AdminDetailByBusiness(ctx *fiber.Ctx) error {
+func (h srv) AdminDetailByBusiness(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func GetBySelectedUser(ctx *fiber.Ctx) error {
+func (h srv) GetBySelectedUser(ctx *fiber.Ctx) error {
 	return nil
 }
 
-func GetBySelectedBusiness(ctx *fiber.Ctx) error {
+func (h srv) GetBySelectedBusiness(ctx *fiber.Ctx) error {
 	return nil
 }
